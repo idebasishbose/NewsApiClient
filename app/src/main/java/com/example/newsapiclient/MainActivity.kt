7b45fef7b8ab1c7fa66d840c4e5c0d069672606d
Debasish Bose <devbose2008@gmail.com>
2021-06-03 21:15:43 +0530

    Recycler View, adapter, DiffUtils

--- app/src/main/java/com/example/newsapiclient/MainActivity.kt
+++ app/src/main/java/com/example/newsapiclient/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.newsapiclient
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsapiclient.databinding.ActivityMainBinding
+import com.example.newsapiclient.presentation.viewmodel.NewsViewModel
+import com.example.newsapiclient.presentation.viewmodel.NewsViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var factory: NewsViewModelFactory
+    lateinit var newsViewModel: NewsViewModel
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +26,6 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bnvNews.setupWithNavController(navController)
+        newsViewModel = ViewModelProvider(this, factory).get(NewsViewModel::class.java)
     }
 }
--- app/src/main/java/com/example/newsapiclient/NewsFragment.kt
+++ app/src/main/java/com/example/newsapiclient/NewsFragment.kt
@@ -1,34 +1,22 @@
 package com.example.newsapiclient
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.newsapiclient.data.util.Resource
+import com.example.newsapiclient.databinding.FragmentNewsBinding
+import com.example.newsapiclient.presentation.adapter.NewsAdapter
+import com.example.newsapiclient.presentation.viewmodel.NewsViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NewsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NewsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var newsAdapter: NewsAdapter
+    private lateinit var viewModel: NewsViewModel
+    private lateinit var fragmentNewsBinding: FragmentNewsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -37,23 +25,52 @@ class NewsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_news, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NewsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NewsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fragmentNewsBinding = FragmentNewsBinding.bind(view)
+        viewModel = (activity as MainActivity).newsViewModel
+        initRecyclerView()
+        viewNewsList()
+    }
+
+    private fun viewNewsList() {
+        viewModel.getNewsHeadlines("us", 1)
+        viewModel.newsHeadlines.observe(viewLifecycleOwner, { response ->
+            when (response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data.let {
+                        newsAdapter.differ.submitList(it?.articles?.toList())
+                    }
+
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message.let {
+                        Toast.makeText(activity, "An error occurred : $it", Toast.LENGTH_SHORT)
+                            .show()
+                    }
                 }
             }
+        })
+    }
+
+    private fun initRecyclerView() {
+        newsAdapter = NewsAdapter()
+        fragmentNewsBinding.rvNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun showProgressBar() {
+        fragmentNewsBinding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        fragmentNewsBinding.progressBar.visibility = View.INVISIBLE
     }
 }
